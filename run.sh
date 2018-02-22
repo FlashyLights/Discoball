bd1be24d7018716f71e6c5da3a2b867c0f198061
James Kinsman <VibroAxe@users.noreply.github.com>
2018-02-22 16:21:14 +0000

    Updated run.sh to git pull all dependencies

--- run.sh
+++ run.sh
@@ -2,6 +2,14 @@
 cd $(dirname $0)
 
 while [ true ]; do
+        git pull
+        pushd modules
+        for f in `ls`; do
+                pushd $f
+                git pull
+                popd
+        done
+        popd
 	echo "Initialising up at" $(date)
 	npm install
 	echo "Starting node..."
