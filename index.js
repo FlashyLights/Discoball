e7fa89d4f9566d49779281a55e7ad36e18b16871
Zoey <crabbey.zl@gmail.com>
2018-12-06 18:24:25 +0000

    newfiles

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) Flashy Lights Ltd 2018 - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+'use strict';
+
+module.exports = require('./lib/discoball');
